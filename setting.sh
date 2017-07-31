f535f9854faa6afed20784608fa1be28e2efb895
Sinpei Araki <speedsolve@speedsolve.local>
2017-07-31 23:55:30 +0900

    first commit

---
+++ setting.sh
@@ -0,0 +1,7 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python
+brew cask install alfred
+brew cask install karabiner-elements
+brew cask install google-japanese-ime
+brew cask install iterm2
+brew cask install google-japanese-ime
