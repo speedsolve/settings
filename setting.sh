336aab73e078004bde151fa57db421c1279159a7
Sinpei Araki <speedsolve@gmail.com>
2017-08-02 22:19:45 +0900

    pythonはpyenvで入れる

--- setting.sh
+++ setting.sh
@@ -1,5 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install python
+brew install zsh
+brew install pyenv
 brew cask install alfred
 brew cask install karabiner-elements
 brew cask install google-japanese-ime
