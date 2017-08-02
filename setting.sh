460400a27a74589380e846650cd0ef28ee1e75df
Sinpei Araki <speedsolve@gmail.com>
2017-08-02 22:35:12 +0900

    pythonやっぱし入れる

--- setting.sh
+++ setting.sh
@@ -1,5 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh
+brew install python
 brew install pyenv
 brew cask install alfred
 brew cask install karabiner-elements
